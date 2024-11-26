24c7843ea9403ec5311506dd7bad87eb7bd8272f
MASM7 <150784893+MASM7@users.noreply.github.com>
2024-11-26 22:26:42 +0300

    Add files via upload

---
+++ cx-203/Practice-Exam/task2/register.sv
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+module register#(
+    parameter width = 8
+  )(
+    input logic [width-1:0] data,
+    input logic clk,
+    input logic reset,
+    input logic en, // enable
+    output logic [width-1:0] q // output
+  );
+
+    always @ (posedge clk, negedge reset) begin    
+//    always @ (*) begin  
+        if (~reset) begin
+          q <= 0; // Reset the output to 0 on negative edge when reset is 0
+        end
+        else if (en) begin
+          q <= data; // Assign data to output q when enabled
+        end
+    end
+
+endmodule
