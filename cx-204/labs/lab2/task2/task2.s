3d5796d11585e597270a33203d4a9a9ee239f54c
MASM7 <150784893+MASM7@users.noreply.github.com>
2024-12-16 16:58:51 +0300

    Add files via upload

---
+++ cx-204/labs/lab2/task2/task2.s
@@ -0,0 +1,19 @@
+.text
+
+#li x5, 0xFFFFFFFF
+#sub x5, x0, x5
+#addi x5, x5, -1
+#add a1, x0, x5
+#addi a0, x0, 1
+#ecall
+
+#li x5, 0x12345678 # 0x12345678 equals 305419896
+#add a1, x0, x5
+#addi a0, x0, 1
+#ecall
+
+li x5, 3
+addi x10, x5, 0x
+add a1, x0, x5
+addi a0, x0, 1
+ecall
